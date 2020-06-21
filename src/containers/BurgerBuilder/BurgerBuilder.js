db360d6f0c5601f4fd6878137c8576b88eec1e9d
Suraj Gahatraj <suraj.gahatraj@itsutra.com>
2020-06-21 08:53:20 +0545

    Project initiated and dynamic compo render added

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,25 @@
+import React ,{Component} from 'react';
+import Auxilliary from '../../hoc/Auxilliary';
+import Burger from '../../components/Burger/Burger'
+class BurgerBuilder extends Component{
+  
+    state={
+        ingredients:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
+    render() {
+        return(
+            <Auxilliary>
+                <Burger ingredients={this.state.ingredients}/>
+                <div>Build Control</div>
+                
+            </Auxilliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
